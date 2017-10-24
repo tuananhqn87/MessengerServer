b12420d807fe02f39c3db3e811cc8b7918ba9e70
Tran Tuan Anh <tuananhqn87@gmail.com>
2017-10-24 14:19:13 +0700

    Created Controller class, rename DBWriter class to DBQuery

--- src/ui/ServerGUI.java
+++ src/ui/ServerGUI.java
@@ -6,14 +6,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -25,11 +17,12 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-import database.DBWriter;
-
-
+import main.Controller;
 
 public class ServerGUI extends JFrame {
+
+	private static final long serialVersionUID = -2255002464550713814L;
+	
 	private final static int DEFAULT_WIDTH = 1;
 	private final static int DEFAULT_HEIGHT = 1;
 	
@@ -41,15 +34,10 @@ public class ServerGUI extends JFrame {
 	private final static int FILL_BOTH = GridBagConstraints.BOTH;
 	private final static int FILL_NONE = GridBagConstraints.NONE;
 	
-	private final static String REGEX = "[0-9]+";
-	
-	private final static String XTER_PREFIX = "xTer: ";
-	private final static String HANNAH_PREFIX = "Hannah: ";
-
 	private JLabel lblServerPort;
 	private JTextField txtServerPort;
 	
-	private JTextArea txtConversation;
+	private static JTextArea txtConversation;
 	private JScrollPane conversationPane;
 	
 	private JTextField txtMessage;
@@ -59,19 +47,11 @@ public class ServerGUI extends JFrame {
 	private JButton btnSend;
 	private UISetup setup;
 	
-	private ServerSocket serverSocket;
-	private Socket socket;
-	private ObjectOutputStream outputStream;
-	private ObjectInputStream inputStream;
-	
-	private int listeningPort;
-//	private int dbId;
-	
 	public ServerGUI (String title) {
 		super(title);
 		setup = new UISetup();
 		initComponents();
-//		dbId = 0;
+
 	}
 	
 	/**
@@ -169,158 +149,28 @@ public class ServerGUI extends JFrame {
 	 * Add behaviors of components
 	 */
 	public void addBehaviors() {
-		enableTyping(false);
-		txtMessage.addActionListener(
-				new ActionListener(){
-					@Override
-					public void actionPerformed(ActionEvent e) {
-						sendMessage(e.getActionCommand());
-						txtMessage.setText("");
-					}
-				});
+		// Initialize event container
+		Controller.EventContainer eventContainer = new Controller.EventContainer();
 		
-		btnListen.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new Thread(){
-					public void run(){
-						String serverPort = txtServerPort.getText();
-						if(serverPort != null && serverPort.matches(REGEX)){
-							int port = Integer.parseInt(serverPort);
-							if(port > 1024 && port <= 65535){
-								setListeningPort(port);
-								startRunning();
-							} else {
-								showMessage("\nYour port number must be between 1024 and 65535");
-								showMessage("\nFailed to start");
-							}
-						} else {
-							showMessage("\nPort has to be a number between 1024 and 65535"
-									+ "\nFailed to start");
-						}
-					}
-				}.start();
-				
-			}
-		});
+		// Add  action listeners to components
+		txtMessage.addActionListener(eventContainer.pressEnterToSend(txtMessage));
 		
-		btnSend.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String message = txtMessage.getText();
-				sendMessage(message);
-				txtMessage.setText("");
-			}
-		});
-	}
-	public void startRunning() {
-		try{
-			serverSocket = new ServerSocket(getListeningPort());
-			while(true){
-				try {
-					DBWriter.delete();
-					waitForConnection();
-					setupStreams();
-					chattingInstance();
-				} catch (EOFException eof){
-					showMessage("\nServer ended the connection! ");
-				} finally {
-					closeConnection();
-				}
-			}
-		} catch (IOException e){
-			txtConversation.setText(txtConversation.getText() + "\n" 
-					+ e.getMessage());
-		}
+		btnListen.addActionListener(eventContainer.clickToListenOnPort(txtServerPort));
+
+		btnSend.addActionListener(eventContainer.clickToSend(txtMessage));
+
 	}
 	
-	/** 
-	 * Wait for connection 
-	 */
-    private void waitForConnection() throws IOException {
-    	showMessage("\nWaiting for someone to connect... ");
-    	socket = serverSocket.accept();
-    	showMessage("\nNow connected to " + socket.getInetAddress().getHostName());
-    }
-
-    /**
-     * Set up streams
-     */
-    private void setupStreams() throws IOException {
-    	// Create output stream from socket
-    	outputStream = new ObjectOutputStream(socket.getOutputStream());
-    	outputStream.flush();
-    	
-    	// Create input stream from socket
-    	inputStream = new ObjectInputStream(socket.getInputStream());
-    }
-    
-    /**
-     * Maintain chatting's instance
-     */
-    private void chattingInstance() throws IOException {
-    	String message = "You are now connected! ";
-    	sendMessage(message);
-    	enableTyping(true);
-    	do{
-    		try{
-    			message = (String) inputStream.readObject();
-    			showMessage("\n" + message);
-    			DBWriter.insert("Xter", "Hannah", message);
-    		} catch (ClassNotFoundException classNotFoundException){
-    			showMessage("\nThe user has sent an unknown object!");
-    			
-    		}
-    	} while(!message.equalsIgnoreCase(XTER_PREFIX + "End"));
-    }
-    
-    /**
+	/**
      * Show the message on conversation window
      */
-    private void showMessage(String message){
-    	SwingUtilities.invokeLater(
-    		new Runnable (){
-    			public void run(){
-    				txtConversation.append(message);
-    			}
-    		});
-    }
-    
-    private void sendMessage(String message){
-    	try {
-    		outputStream.writeObject(HANNAH_PREFIX + message);
-    		outputStream.flush();
-    		DBWriter.insert("Hannah", "Xter", message);
-    		showMessage("\nMe: " + message);
-    	} catch(IOException ioException){
-    		txtConversation.append("\nERROR: CANNOT SEND MESSAGE, PLEASE RETRY!");
-    	}
-    }
-    /** Close the socket */
-    public void closeConnection() {
-        try {
-        	outputStream.close();
-        	inputStream.close();
-            socket.close();
-        } catch (IOException e) {
-        	e.printStackTrace();
-        }
-    }
-	
-    private void enableTyping(boolean isEnabled){
-    	SwingUtilities.invokeLater(
-			new Runnable(){
-				public void run(){
-					txtMessage.setEditable(isEnabled);
-				}
-			});
-    }
-    
-    public void setListeningPort(int port){
-    	this.listeningPort = port;
-    }
-    
-    public int getListeningPort(){
-    	return listeningPort;
+    public static void showMessage(String message){
+	    	SwingUtilities.invokeLater(
+	    		new Runnable (){
+	    			public void run(){
+	    				txtConversation.append("\n");
+	    				txtConversation.append(message);
+	    			}
+	    		});
     }
 }
